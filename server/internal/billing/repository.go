14b822889cb311482e65a47702a19f942e345a01
yinxuea <1621535815@qq.com>
2026-04-24 23:21:36 +0800

    1

--- server/internal/billing/repository.go
+++
@@ -1,3 +0,0 @@
-// Repository is defined in service.go to keep the service-repository contract
-// alongside the service that consumes it.
-package billing
--- web/src/views/LoginView.test.ts
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/vue'
-import { expect, test } from 'vitest'
-import LoginView from './LoginView.vue'
-
-test('renders login form fields', () => {
-  render(LoginView)
-  expect(screen.getByLabelText('手机号')).toBeTruthy()
-  expect(screen.getByLabelText('密码')).toBeTruthy()
-})
